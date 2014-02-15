0bd39616971937a8c62d305ac1f9687600586831
Shota Nozaki <emonkak@gmail.com>
2014-02-15 21:37:06 +0900

    Implement 'getCurrentUserHandle'

---
+++ Web/Skype/Command/User.hs
@@ -0,0 +1,23 @@
+module Web.Skype.Command.User (
+  getCurrentUserHandle
+) where
+
+import Control.Monad.Trans (MonadIO)
+import Data.Monoid ((<>))
+import Web.Skype.Command.Utils
+import Web.Skype.Core
+import Web.Skype.Parser
+import Web.Skype.Protocol.User
+import Web.Skype.Protocol.Types
+
+import qualified Data.ByteString as BS
+
+getCurrentUserHandle :: (MonadIO m, MonadSkype m) => SkypeT m UserID
+getCurrentUserHandle = executeCommandWithID command $ \response ->
+  case response of
+    CurrentUserHandle userID -> Just $ Right userID
+    Error code description   -> Just $ Left $ SkypeError code command description
+    _                        -> Nothing
+  where
+    command = "GET CURRENTUSERHANDLE"
+
